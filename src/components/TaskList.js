c11c46804bed5e091c4ad584dd93a10e19a5e713
Ole Skaar <ole.skaar@geta.no>
2020-08-24 13:51:03 +0200

    Changed text on TaskList - Empty story as a test.

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -40,7 +40,7 @@ export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
         <div className="wrapper-message">
           <span className="icon-check" />
           <div className="title-message">You have no tasks</div>
-          <div className="subtitle-message">Sit back and relax</div>
+          <div className="subtitle-message">Would you like to add one?</div>
         </div>
       </div>
     );
