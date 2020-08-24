f8226f329d6c767c051a0930b0aad5ff38b78241
Ole Skaar <ole.skaar@geta.no>
2020-08-24 13:26:02 +0200

    Taskbox UI.

---
+++ storybook-static/precache-manifest.5d3ea368e469d74bff903400b4f720c5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aea140e126c1437a279255994a963755",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2d8972f4c58814fe968f.bundle.js"
+  },
+  {
+    "url": "runtime~main.2d8972f4c58814fe968f.bundle.js"
+  },
+  {
+    "revision": "c9397444399ba44e54ab",
+    "url": "static/css/main.446d8ff6.chunk.css"
+  },
+  {
+    "url": "vendors~main.2d8972f4c58814fe968f.bundle.js"
+  },
+  {
+    "url": "vendors~main.2d8972f4c58814fe968f.bundle.js.LICENSE.txt"
+  }
+]);
